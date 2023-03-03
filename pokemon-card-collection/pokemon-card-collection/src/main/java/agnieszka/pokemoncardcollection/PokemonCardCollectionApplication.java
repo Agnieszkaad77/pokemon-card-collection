96e44a28a0524ead0c63206a514a89dba5456ca4
Agnieszka <agnieszka.ad77@gmail.com>
2023-03-03 21:58:57 +0100

    Start

---
+++ pokemon-card-collection/pokemon-card-collection/src/main/java/agnieszka/pokemoncardcollection/PokemonCardCollectionApplication.java
@@ -0,0 +1,13 @@
+package agnieszka.pokemoncardcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCardCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonCardCollectionApplication.class, args);
+	}
+
+}
